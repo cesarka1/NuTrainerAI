ed908880ee3df54106fc14e5ba0e6be7c002be52
Kauan <cesarkauanferreira@hotmail.com>
2024-09-04 23:04:31 -0300

    feat; Seção de Ajuda

---
+++ js/app.js
@@ -0,0 +1,105 @@
+function criarElementoResultado(titulo, descricao, grupoMuscular, nivel, objetivo, link, exerciciosOuPratos) {
+    return `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" class="mais-informacoes" data-titulo="${titulo}" data-conteudo='${JSON.stringify(exerciciosOuPratos)}'>${titulo}</a>
+            </h2>
+            <p class="descricao-meta">${descricao}</p>
+            ${grupoMuscular ? `<p><strong>Grupo Muscular:</strong> ${grupoMuscular}</p>` : ''}
+            ${nivel ? `<p><strong>Nível:</strong> ${nivel}</p>` : ''}
+            ${objetivo ? `<p><strong>Objetivo:</strong> ${objetivo}</p>` : ''}
+            <a href="#" class="mais-informacoes" data-titulo="${titulo}" data-conteudo='${JSON.stringify(exerciciosOuPratos)}'>Mais informações</a>
+        </div>
+    `;
+}
+
+function gerarResultados() {
+    let resultadosHTML = '';
+
+    // Adicionando treinos à seção
+    treinos.forEach(treino => {
+        resultadosHTML += criarElementoResultado(
+            treino.Titulo,
+            treino.Descricao,
+            treino.GrupoMuscular,
+            treino.Nivel,
+            '', // Objetivo não se aplica aqui
+            '#', // Altere '#' para um link relevante
+            treino.Exercicios // Passa o array de objetos
+        );
+    });
+
+    // Adicionando dietas à seção
+    dietas.forEach(dieta => {
+        resultadosHTML += criarElementoResultado(
+            dieta.Titulo,
+            dieta.Descricao,
+            '', // Grupo Muscular não se aplica aqui
+            '', // Nível não se aplica aqui
+            dieta.Objetivo,
+            '#', // Altere '#' para um link relevante
+            dieta.Refeicoes // Passa o array de objetos
+        );
+    });
+
+    // Inserindo o HTML gerado na seção
+    document.getElementById('resultados').innerHTML = resultadosHTML;
+
+    // Adiciona evento de clique nos links "Mais informações"
+    const maisInformacoesLinks = document.querySelectorAll('.mais-informacoes');
+    maisInformacoesLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const titulo = e.target.getAttribute('data-titulo');
+            const conteudo = JSON.parse(e.target.getAttribute('data-conteudo')); // Converte de volta para objeto
+            abrirModal(titulo, conteudo);
+        });
+    });
+}
+
+function abrirModal(titulo, conteudo) {
+    document.getElementById('modalTitulo').innerText = titulo;
+
+    // Formata o conteúdo do modal
+    let conteudoHTML = '<ul>';
+    conteudo.forEach(item => {
+        conteudoHTML += `<div class="item-resultado"><strong>${item.Nome}
+        </strong>: 
+        
+        ${item.Series ? `<p><strong>Series:</strong> ${item.Series}</p>` : ''}
+        ${item.Repeticoes ? `<p><strong>Repeticoes:</strong> ${item.Repeticoes}</p>` : ''}
+        ${item.Descanso ? `<p><strong>Descanso:</strong> ${item.Descanso}</p>` : ''}
+        ${item.Equipamento ? `<p><strong>Equipamento:</strong> ${item.Equipamento}</p>` : ''}
+
+        
+        ${item.Alimentos ? `<p><strong>Alimentos:</strong> ${item.Alimentos}</p>` : ''}
+        ${item.Calorias ? `<p><strong>Calorias:</strong> ${item.Calorias}</p>` : ''}
+        ${item.Macros ? `<p><strong>Macros:</strong> 
+            <strong>Carboidratos: </strong>${item.Macros.carboidratos}
+              <strong>Proteinas: </strong>${item.Macros.proteinas}
+                <strong>Gorduras: </strong>${item.Macros.gorduras}
+            </p>` : ''}
+        </div>`
+       
+        ;
+    });
+    conteudoHTML += '</ul>';
+
+    document.getElementById('modalConteudo').innerHTML = conteudoHTML;
+    document.getElementById('myModal').style.display = "block";
+}
+
+// Fecha o modal quando o usuário clica no "x"
+document.querySelector('.close').onclick = function() {
+    document.getElementById('myModal').style.display = "none";
+}
+
+// Fecha o modal quando o usuário clica fora do modal
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = "none";
+    }
+}
+
+// Chama a função para gerar os resultados após o carregamento do DOM
+document.addEventListener('DOMContentLoaded', gerarResultados);
---
+++ js/dados.js
@@ -0,0 +1,563 @@
+let treinos = [
+    {
+        Titulo: "Treino de Peito para Iniciantes",
+        Descricao: "Treino focado no desenvolvimento dos músculos peitorais para iniciantes.",
+        GrupoMuscular: "Peito",
+        Nivel: "Iniciante",
+        Exercicios: [
+            {
+                Nome: "Supino Reto com Barra",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Barra e Halteres"
+            },
+            {
+                Nome: "Supino Inclinado com Halteres",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Flexões",
+                Series: 3,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Peso Corporal"
+            },
+            {
+                Nome: "Peck Deck",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Crossover",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Cabos"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Peito para Intermediários",
+        Descricao: "Treino avançado para aumento de força e hipertrofia no peito.",
+        GrupoMuscular: "Peito",
+        Nivel: "Intermediário",
+        Exercicios: [
+            {
+                Nome: "Supino Reto com Barra",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Supino Inclinado com Halteres",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Crucifixo Inclinado com Halteres",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Peck Deck",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Crossover",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Cabos"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Peito para Avançados",
+        Descricao: "Treino intenso para maximizar a hipertrofia e definição dos músculos peitorais.",
+        GrupoMuscular: "Peito",
+        Nivel: "Avançado",
+        Exercicios: [
+            {
+                Nome: "Supino Reto com Barra",
+                Series: 5,
+                Repeticoes: "6-8",
+                Descanso: 120,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Supino Inclinado com Halteres",
+                Series: 5,
+                Repeticoes: "6-8",
+                Descanso: 120,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Supino Declinado com Barra",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Crucifixo com Cabos",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Cabos"
+            },
+            {
+                Nome: "Peck Deck com Dropset",
+                Series: 4,
+                Repeticoes: "12-15",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Pernas para Iniciantes",
+        Descricao: "Treino básico para iniciantes fortalecerem as pernas.",
+        GrupoMuscular: "Pernas",
+        Nivel: "Iniciante",
+        Exercicios: [
+            {
+                Nome: "Agachamento Livre",
+                Series: 3,
+                Repeticoes: "12-15",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Leg Press",
+                Series: 3,
+                Repeticoes: "12-15",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Cadeira Extensora",
+                Series: 3,
+                Repeticoes: "12-15",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Cadeira Flexora",
+                Series: 3,
+                Repeticoes: "12-15",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Elevação de Panturrilha",
+                Series: 3,
+                Repeticoes: "15-20",
+                Descanso: 45,
+                Equipamento: "Peso Corporal"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Pernas para Avançados",
+        Descricao: "Treino para avançados focado na hipertrofia e força das pernas.",
+        GrupoMuscular: "Pernas",
+        Nivel: "Avançado",
+        Exercicios: [
+            {
+                Nome: "Agachamento Livre com Peso",
+                Series: 5,
+                Repeticoes: "6-8",
+                Descanso: 120,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Leg Press com Dropset",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Cadeira Extensora com Isometria",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Cadeira Flexora com Isometria",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Levantamento Terra com Barra",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Barra"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Costas para Iniciantes",
+        Descricao: "Treino básico para iniciantes desenvolverem as costas.",
+        GrupoMuscular: "Costas",
+        Nivel: "Iniciante",
+        Exercicios: [
+            {
+                Nome: "Puxada Aberta com Barra",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Remada Curvada com Barra",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Remada Unilateral com Halteres",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Puxada na Máquina",
+                Series: 3,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Barra Fixa Assistida",
+                Series: 3,
+                Repeticoes: "8-10",
+                Descanso: 90,
+                Equipamento: "Máquina"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Costas para Avançados",
+        Descricao: "Treino desafiador para aumentar a massa muscular das costas.",
+        GrupoMuscular: "Costas",
+        Nivel: "Avançado",
+        Exercicios: [
+            {
+                Nome: "Puxada Aberta com Barra",
+                Series: 5,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Remada Curvada com Barra",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Remada Unilateral com Halteres",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Puxada na Máquina",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            },
+            {
+                Nome: "Barra Fixa com Peso Adicional",
+                Series: 4,
+                Repeticoes: "6-8",
+                Descanso: 90,
+                Equipamento: "Peso Corporal"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Ombros para Intermediários",
+        Descricao: "Treino para intermediários desenvolverem força e definição nos ombros.",
+        GrupoMuscular: "Ombros",
+        Nivel: "Intermediário",
+        Exercicios: [
+            {
+                Nome: "Elevação Lateral com Halteres",
+                Series: 4,
+                Repeticoes: "12-15",
+                Descanso: 45,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Desenvolvimento Militar com Barra",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Elevação Frontal com Halteres",
+                Series: 4,
+                Repeticoes: "12-15",
+                Descanso: 45,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Encolhimento de Ombros com Barra",
+                Series: 4,
+                Repeticoes: "12-15",
+                Descanso: 45,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Rotação Externa com Halteres",
+                Series: 4,
+                Repeticoes: "12-15",
+                Descanso: 45,
+                Equipamento: "Halteres"
+            }
+        ]
+    },
+    {
+        Titulo: "Treino de Braços para Avançados",
+        Descricao: "Treino avançado para maximizar a hipertrofia de bíceps e tríceps.",
+        GrupoMuscular: "Braços",
+        Nivel: "Avançado",
+        Exercicios: [
+            {
+                Nome: "Rosca Direta com Barra",
+                Series: 5,
+                Repeticoes: "6-8",
+                Descanso: 90,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Tríceps Testa com Barra",
+                Series: 5,
+                Repeticoes: "6-8",
+                Descanso: 90,
+                Equipamento: "Barra"
+            },
+            {
+                Nome: "Rosca Alternada com Halteres",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Halteres"
+            },
+            {
+                Nome: "Mergulho entre bancos com Peso",
+                Series: 4,
+                Repeticoes: "8-10",
+                Descanso: 60,
+                Equipamento: "Peso Corporal"
+            },
+            {
+                Nome: "Rosca Scott com Isometria",
+                Series: 4,
+                Repeticoes: "10-12",
+                Descanso: 60,
+                Equipamento: "Máquina"
+            }
+        ]
+    }
+];
+
+let dietas = [
+    {
+        Titulo: "Dieta para Vegetarianos",
+        Descricao: "Dieta balanceada para vegetarianos, rica em proteínas vegetais.",
+        Objetivo: "Manutenção",
+        Refeicoes: [
+            {
+                Nome: "Almoço",
+                Alimentos: ["Lentilha", "Arroz integral", "Salada verde", "Tofu grelhado", "Brócolis"],
+                Calorias: 500,
+                Macros: {
+                    carboidratos: 60,
+                    proteinas: 25,
+                    gorduras: 15
+                }
+            },
+            {
+                Nome: "Jantar",
+                Alimentos: ["Quibe de quinoa", "Legumes salteados", "Suco de laranja", "Abacate", "Arroz de couve-flor"],
+                Calorias: 450,
+                Macros: {
+                    carboidratos: 50,
+                    proteinas: 20,
+                    gorduras: 20
+                }
+            },
+            {
+                Nome: "Café da Manhã",
+                Alimentos: ["Iogurte natural", "Granola", "Frutas vermelhas", "Sementes de chia", "Mel"],
+                Calorias: 350,
+                Macros: {
+                    carboidratos: 50,
+                    proteinas: 15,
+                    gorduras: 10
+                }
+            },
+            {
+                Nome: "Lanche da Tarde",
+                Alimentos: ["Hummus", "Palitos de cenoura", "Torradas integrais", "Frutas secas", "Castanhas"],
+                Calorias: 300,
+                Macros: {
+                    carboidratos: 40,
+                    proteinas: 10,
+                    gorduras: 15
+                }
+            },
+            {
+                Nome: "Ceia",
+                Alimentos: ["Chá de ervas", "Biscoitos integrais", "Fruta fresca", "Queijo vegano", "Amêndoas"],
+                Calorias: 250,
+                Macros: {
+                    carboidratos: 30,
+                    proteinas: 5,
+                    gorduras: 12
+                }
+            }
+        ]
+    },
+    {
+        Titulo: "Dieta para Ganho de Massa Muscular",
+        Descricao: "Dieta rica em proteínas e carboidratos para auxiliar no crescimento muscular.",
+        Objetivo: "Ganho de Massa",
+        Refeicoes: [
+            {
+                Nome: "Pré-treino",
+                Alimentos: ["Banana", "Aveia", "Proteína em pó", "Manteiga de amendoim", "Leite desnatado"],
+                Calorias: 300,
+                Macros: {
+                    carboidratos: 50,
+                    proteinas: 20,
+                    gorduras: 8
+                }
+            },
+            {
+                Nome: "Pós-treino",
+                Alimentos: ["Shake de proteína com frutas", "Aveia", "Iogurte grego", "Mel", "Sementes de linhaça"],
+                Calorias: 250,
+                Macros: {
+                    carboidratos: 30,
+                    proteinas: 25,
+                    gorduras: 5
+                }
+            },
+            {
+                Nome: "Café da Manhã",
+                Alimentos: ["Ovos mexidos", "Pão integral", "Abacate", "Suco de laranja", "Queijo"],
+                Calorias: 500,
+                Macros: {
+                    carboidratos: 40,
+                    proteinas: 30,
+                    gorduras: 20
+                }
+            },
+            {
+                Nome: "Almoço",
+                Alimentos: ["Frango grelhado", "Arroz branco", "Feijão", "Salada de folhas", "Batata doce"],
+                Calorias: 600,
+                Macros: {
+                    carboidratos: 70,
+                    proteinas: 40,
+                    gorduras: 15
+                }
+            },
+            {
+                Nome: "Jantar",
+                Alimentos: ["Salmão grelhado", "Quinoa", "Legumes assados", "Azeite de oliva", "Espinafre"],
+                Calorias: 550,
+                Macros: {
+                    carboidratos: 45,
+                    proteinas: 35,
+                    gorduras: 20
+                }
+            }
+        ]
+    },
+    {
+        Titulo: "Dieta para Emagrecimento",
+        Descricao: "Dieta hipocalórica para auxiliar na perda de peso.",
+        Objetivo: "Emagrecimento",
+        Refeicoes: [
+            {
+                Nome: "Café da Manhã",
+                Alimentos: ["Aveia", "Frutas", "Leite desnatado", "Sementes de girassol", "Mel"],
+                Calorias: 300,
+                Macros: {
+                    carboidratos: 50,
+                    proteinas: 10,
+                    gorduras: 5
+                }
+            },
+            {
+                Nome: "Almoço",
+                Alimentos: ["Peito de frango grelhado", "Salada de rúcula", "Quinoa", "Legumes crus", "Azeite de oliva"],
+                Calorias: 400,
+                Macros: {
+                    carboidratos: 30,
+                    proteinas: 40,
+                    gorduras: 10
+                }
+            },
+            {
+                Nome: "Lanche da Tarde",
+                Alimentos: ["Iogurte natural", "Frutas", "Granola", "Sementes de chia", "Canela"],
+                Calorias: 200,
+                Macros: {
+                    carboidratos: 30,
+                    proteinas: 10,
+                    gorduras: 5
+                }
+            },
+            {
+                Nome: "Jantar",
+                Alimentos: ["Sopa de legumes", "Tofu grelhado", "Salada de alface", "Abacate", "Limão"],
+                Calorias: 350,
+                Macros: {
+                    carboidratos: 40,
+                    proteinas: 20,
+                    gorduras: 10
+                }
+            },
+            {
+                Nome: "Ceia",
+                Alimentos: ["Chá de ervas", "Biscoitos integrais", "Fruta fresca", "Iogurte grego", "Amêndoas"],
+                Calorias: 150,
+                Macros: {
+                    carboidratos: 20,
+                    proteinas: 5,
+                    gorduras: 5
+                }
+            }
+        ]
+    },
+];
